2ea2b42215ba4ada7190efcc58a92d1b0d8d6388
SquanchyJr <squanchyjr@pc-206-25.customer.mileend.grosvenor.lan>
2016-12-01 06:14:03 +0000

    Exercise 8 completed with comments

--- ProcProgEx/8_BonusScheme/BonusScheme.java
+++ ProcProgEx/8_BonusScheme/BonusScheme.java
@@ -2,56 +2,57 @@ import java.util.Scanner;
 
 public class BonusScheme {
     public static void main( String[] param ) {
-        System.out.println( "Profit Score");
+        System.out.println( "Profit Score"); //Ask user for 2 variable
         int profit_score = usrInput();    
 
         System.out.println( "Hard Work Score?");
         int hardwork_score = usrInput();
 
-        int bonus_val = profitBonus( profitscore );
-        int bonus_val += hardWorkBonus( hardwork_score );   
+        int performance_score = performanceScore( profit_score, hardwork_score );
+        System.out.println( "Your Performance Score this year is "+  performance_score +" out of 10" );
+   
+        int bonus = finalBonus( performance_score );
+        System.out.println( "Your bonus is £"+ bonus + ".");
     }
-    public static int usrInput() {
+    public static int usrInput() { //Method for user input
         Scanner sc = new Scanner( System.in );
         int input = sc.nextInt();
         return input;
     }
 
-    public static int profitBonus( int param )
+    public static int profitBonus( int param ) //returns bonus for profit variable
     {
-        profitscore = param * 2;
+        int profitscore = param * 2;
         return profitscore;
     }
     
-    public static int hardWorkBonus( int param) 
+    public static int hardWorkBonus( int param) //returns bonus for hardwork variable 
     {
-        hardwork_score = param * 5
-    }
-
-    public static void hhh(){}
-}
-
-class Employee {
-
-    public int profit_score = 0;
-    public int hardwork_score = 0;
-
-    public int getProfit( ) 
-    {
-        return profit_score;
-    }
-    public int getHardWork() 
-    {
+        int hardwork_score = param * 5;
         return hardwork_score;
     }
 
-    public void setProfit( int param ) 
+    public static int performanceScore( int profit, int hardwork ) // returns overall bonus multiplier for the profit and hardwork variables
     {
-        profit_score = param;
-    }
-    public void setHardWork( int param ) 
+        int profitscore = profitBonus( profit );
+        int hardworkscore = hardWorkBonus( hardwork );
+
+        int finalscore = profitscore + hardworkscore;
+        
+        return finalscore / 7; 
+    } // shouldnt this be a float value??
+
+    public static int finalBonus( int param ) // returns final bonus which is the performance score multiplied by 5000
     {
-        hardwork_score = param;
-    }
+        int bonus = param * 5000;
+        return bonus; 
+   }
+}
+
+class Employee //employee variables which default to zero. 
+{
+                
+    public int profit_score = 0;
+    public int hardwork_score = 0;
 
 }
