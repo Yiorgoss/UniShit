71269002ce913253741c1730c70a1355680c2bf6
SquanchyJr <squanchyjr@SquanchyJrs-MacBook-Pro.local>
2016-12-05 11:39:33 +0000

    comments added.

--- ProcProgEx/7_whileLoop/HowMany.java
+++ ProcProgEx/7_whileLoop/HowMany.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class HowMany {
 
-	public static void main(String[] p){
+	public static void main( String[] p ){
 		Scanner sc = new Scanner(System.in);
 		String usr_str = "";
 		String commonbird = "";
@@ -10,13 +10,19 @@ class HowMany {
 		int usr_int;	
 		int mostcommon = 0;
 		
-//		while(!(usr_str.equals("end"))) {
-		while(true){	
-			System.out.println("Which bird have you seen?");
+		while( true ){
+        // while condition = true ( always )	
+		//ask user what bird was seen
+        //record input
+        // ask user how many were seen
+        // record number of birds seen
+        // output the greatest number of bird seen
+        // only if user inputs end
+        	System.out.println( "Which bird have you seen?" );
 			usr_str = sc.nextLine();
 			
-			if(usr_str.equals("end")){
-				System.out.println("You saw "+ mostcommon +" "+ commonbird +"s");
+			if(usr_str.equals( "end" ) ){
+				System.out.println( "You saw "+ mostcommon +" "+ commonbird +"s" );
 				break;
 			} else {
 		
