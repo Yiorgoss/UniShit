b685b66ecec8d644875c3a6bbf097508395386ea
SquanchyJr <squanchyjr@SquanchyJrs-MacBook-Pro.local>
2016-12-05 11:32:51 +0000

    finished with some comments as well

--- ProcProgEx/6_arrays/HowMany.java
+++ ProcProgEx/6_arrays/HowMany.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 class HowMany {
 	
 	public static void main(String[] p){
-		
+		// create array of animals
+		// call method animal ask which will run through array of animals and ask user what the population of each animal is.
+		String[] animal_arr = new String[] {"Komodo Dragon","Manatee","Kakapo","Florida Panther", "White Rhino"};
+        
+        animalAsk( animal_arr );
+	}
+    public static void animalAsk( String[] animal_arr)
+    {
+    // ask about different animals
+    // save user input
+    // compare user input and only output the smallest animal. ie the most endangered.
 		Scanner sc = new Scanner(System.in);
 		
-		String[] animal_arr = new String[] {"Komodo Dragon","Manatee","Kakapo","Florida Panther", "White Rhino"};
-		int[] popul_arr = new int[5];
-		
+        int[] popul_arr = new int[5];
 		int lowest = 1000000000;
 		int most_enda = 0;
-
-		for(int i=0; i<=4; i++){
-
+        int n = animal_arr.length;
+        for( int i=0; i<=n-1; i++ ){ //
+                    
 			System.out.println(animal_arr[i] +":");
 			System.out.println("How many are left in the wild?");
 			
@@ -25,7 +33,6 @@ class HowMany {
 				lowest = usr_inp;
 			}
 
-System.out.println(lowest);
 			if(i==4){
 				System.out.println("The most endangered animal is the "+ animal_arr[most_enda]);
 				System.out.println("There are only "+ popul_arr[most_enda] +" left in the wild");
@@ -35,5 +42,5 @@ System.out.println(lowest);
 				}
 			}
 		}		
-	}
+    }
 }
