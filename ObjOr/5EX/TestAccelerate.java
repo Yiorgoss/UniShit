8cf5993dbf39992c3c37c2d4bdd1b36f4f96c7e4
SquanchyJr <squanchyjr@SquanchyJrs-MacBook-Pro.local>
2017-02-27 13:29:47 +0000

    exercise 5 completed

--- ObjOr/5EX/TestAccelerate.java
+++ ObjOr/5EX/TestAccelerate.java
@@ -1,15 +1,21 @@
 public class TestAccelerate {
 
     public static void main( String[] args ) {
-        SportsCar sc1 = new SportsCar( 1500, 200, 0.5, 220 );
-        SportsCar sc2 = new SportsCar( 1000, 100, 0.5, 170 );
-        SportsCar sc3 = new SportsCar( 1100.2, 135, 0.5, 173 );
-
-        Van van = new Van( 3500, 100, 0.9, 160.4 );
+        Vehicle[] automobiles = new Vehicle[4];
         
-        System.out.println( "Acceleration for sportscar 1 = "+ sc1.acceleration() );
-        System.out.println( "Acceleration for sportscar 2 = "+ sc2.acceleration() );
-        System.out.println( "Acceleration for sportscar 3 = "+ sc3.acceleration() );
-        System.out.println( "Acceleration for Van = "+ van.acceleration() );
+        automobiles[0] = new SportsCar( 1500, 200, 0.5, 220 );
+        automobiles[1] = new SportsCar( 1000, 100, 0.5, 170 );
+        automobiles[2] = new SportsCar( 1100.2, 135, 0.5, 173 );
+
+        automobiles[3] = new Van( 3500, 100, 0.9, 160.4 );
+        for( int i=0; i<4; i++ ) {
+            if( automobiles[i] instanceof Van ) {
+                System.out.println( "Acceleration for Van = "+ automobiles[i].acceleration() );
+            } else if( automobiles[i] instanceof SportsCar ) {    
+                System.out.println( "Acceleration for sportscar "+ (i+1) +" = "+ automobiles[i].acceleration() );
+            } else {
+                System.out.println( "ERROR" );
+            }
+        }
     }
 }
