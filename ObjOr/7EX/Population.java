8286e4aedeb0ea80505208660e849b9f752d7fe2
SquanchyJr <squanchyjr@pc-206-25.customer.mileend.grosvenor.lan>
2017-03-16 11:43:07 +0000

    finished all short exs

--- ObjOr/7EX/Population.java
+++ ObjOr/7EX/Population.java
@@ -1,17 +1,36 @@
 import java.io.*;
+import java.util.*;
 
 public class Population {
     public static void main( String[] args ) {
+        int param = Integer.parseInt( args[0] );
+        int c = greaterThan( inputFile( "textfile.txt" ), param );
         
+        System.out.println( c );
     }
-    public static void inputFile( String arg ) throws IOException {
-        FileInputStream in = null;
-
+    public static ArrayList<Integer> inputFile( String fname ) {
+        ArrayList<Integer> lines = new ArrayList<Integer>();         
         try {
-            inFile = new FileInputStream( "textfile.txt" );
-            
-            while ( inFile.hasNext() ) { 
-                  
-            }      
+            BufferedReader input = new BufferedReader( new FileReader( fname ));    
+            String c = "";
+            while((c = input.readLine()) != null ) {
+                String[] tarray = c.split( " " );
+                lines.add( Integer.parseInt( tarray[1] ));
+            }
+        } catch( Exception e ) {
+
+        }
+        return lines;
+    }
+    
+    public static int greaterThan( ArrayList<Integer> input, int n ) {
+        int j =0;
+
+        for( int i=0; i<input.size(); i++ ) {
+            if( input.get(i) >= n ) {
+                j++;
+            }                          
+        }   
+        return j;
     }
 }
